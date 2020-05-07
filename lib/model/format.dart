1f783c334c60660b7c6c8a993d5db808c68d187f
Dan <oddestdan@gmail.com>
2020-05-08 00:20:13 +0300

    Create 3D file format model

---
+++ lib/model/format.dart
@@ -0,0 +1,21 @@
+class Format {
+  String format;
+  String title;
+  String mainInfo;
+  String description;
+  double popularity;
+  bool isProprietary;
+  String useCase;
+  String learnMore;
+
+  Format({
+    this.format,
+    this.title,
+    this.mainInfo,
+    this.description,
+    this.popularity,
+    this.isProprietary,
+    this.useCase,
+    this.learnMore,
+  });
+}
